2b3a14851d29bd6585e3d3660e8329b53124c36e
caiht <caihuatao@qianbaocard.com>
2018-03-26 23:34:56 +0800

    通过字符串截取的方法将author、tag、time、publisher所要内容成功截取出

--- src/main/java/sciencenet/SciencenetCrawler.java
+++ src/main/java/sciencenet/SciencenetCrawler.java
@@ -8,9 +8,24 @@ import org.apache.commons.lang.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.UnsupportedEncodingException;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class SciencenetCrawler extends WebCrawler {
 
+    private final static Pattern FILTERS = Pattern.compile("\\d{4}/(\\d{1,2})/(\\d{1,2})");
+
+    // JDBC 驱动名及数据库 URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/science";
+
+    // 数据库的用户名与密码，需要根据自己的设置
+    static final String USER = "root";
+    static final String PASS = "";
 
     /**
      * This method receives two parameters. The first parameter is the page
@@ -40,27 +55,57 @@ public class SciencenetCrawler extends WebCrawler {
         if (page.getParseData() instanceof HtmlParseData) {
             HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
             String html = htmlParseData.getHtml();
+//            try {
+//                html = new String(html.getBytes("gbk"),"utf-8");
+//            } catch (UnsupportedEncodingException e) {
+//                e.printStackTrace();
+//            }
             Document doc = Jsoup.parse(html);
-            String title =doc.select("#ct > div.hmleft > div.hmleft1 > h1").text();
+            String title = doc.select("#ct > div.hmleft > div.hmleft1 > h1").text();
             System.out.println("title: " + title);
-            String author =doc.select("#ct > div.hmleft > div:nth-child(3)").text();
+            String content = doc.select("#ct > div.hmleft > div:nth-child(3)").text();
 
 
-
-            author= StringUtils.substringBefore(author.substring(4), "期刊名称");
+            String author = StringUtils.substringBetween(content, "作 者：", "期刊名称");
             System.out.println("author: " + author);
 
-            String time =doc.select("#ct > div.hmleft > div.hmleft1").text();
+            String time = doc.select("#ct > div.hmleft > div.hmleft1").text();
+            Matcher matcher = FILTERS.matcher(time);
+            if (matcher.find())
+                time = matcher.group();
             System.out.println("time: " + time);
 
-            String publisher =doc.select("#ct > div.hmleft > div:nth-child(3) > br:nth-child(4)").text();
+            String publisher = StringUtils.substringBetween(content, "期刊名称：", "期卷页");
             System.out.println("publisher: " + publisher);
 
-            String tag =doc.select("#ct > div.hmleft > div:nth-child(3) > br:nth-child(8)").text();
+            String tag = StringUtils.substringBetween(content, "学科领域：", "添加人是否为作者");
             System.out.println("tag: " + tag);
 
-            String introduction =doc.select("#ct > div.hmleft > div.hmleft1 > p").text();
+            String introduction = doc.select("#ct > div.hmleft > div.hmleft1 > p").text();
             System.out.println("introduction: " + introduction);
+            Connection conn = null;
+            Statement stmt = null;
+
+            // 注册 JDBC 驱动
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+                Date newDate = new Date(sDateFormat.parse(time).getTime());
+                String template = " INSERT INTO articles VALUES(?,?,?,?,?,?)";
+                PreparedStatement statement = conn.prepareStatement(template);
+                statement.setString(1,title);
+                statement.setString(2,author);
+                statement.setDate(3, newDate);
+                statement.setString(4,publisher);
+                statement.setString(5,tag);
+                statement.setString(6,introduction);
+                statement.execute();
+                System.out.println("成功插入数据");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
 
         }
     }
